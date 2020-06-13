adb04949fe0856074fd08079b0b64048c5f5c6bf
JBernal <jon@bluesandswebdesign.com>
2020-06-12 18:11:40 -0700

    -

--- src/components/slateEditor/slateEditor.js
+++ src/components/slateEditor/slateEditor.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState, useCallback } from "react";
 
-import { createEditor, Transforms, Editor } from "slate";
+import { createEditor, Transforms, Editor, Text } from "slate";
 
 import { Slate, Editable, withReact } from "slate-react";
 
@@ -15,15 +15,6 @@ const SlateEditor = () => {
     },
   ]);
 
-  const fixAnd = (e) => {
-    if (e.key === "&") {
-      // prevent the ampersand character from being inserted
-      e.preventDefault();
-      // Exeute the 'insertText' method when the event occurs to insert and instead of &.
-      editor.insertText("and");
-    }
-  };
-
   // Define a rendering function based on the element passed to `props`. we use `useCallback` here to memoize the function for subsequent renders.
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
@@ -35,17 +26,23 @@ const SlateEditor = () => {
     }
   }, []);
 
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
+
   const elementSelector = (e) => {
+    if (!e.ctrlKey) return;
     e.preventDefault();
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.type === "code",
-    });
-    if (e.key === "`" && e.ctrlKey) {
-      Transforms.setNodes(
-        editor,
-        { type: match ? "paragraph" : "code" },
-        { match: (n) => Editor.isBlock(editor, n) }
-      );
+
+    switch (e.key) {
+      case "`": {
+        CustomEditor.toggleCodeBlock(editor);
+        break;
+      }
+      case "b": {
+        CustomEditor.toggleBoldMark(editor);
+        break;
+      }
     }
   };
 
@@ -56,9 +53,28 @@ const SlateEditor = () => {
       value={value}
       onChange={(newValue) => setValue(newValue)}
     >
+      <div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            CustomEditor.toggleBoldMark(editor);
+          }}
+          style={{ fontWeight: "bold" }}
+        >
+          B
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            CustomEditor.toggleCodeBlock(editor);
+          }}
+        >
+          Code Block
+        </button>
+      </div>
       <Editable
-        onKeyDown={(e) => fixAnd(e)}
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
         onKeyUp={(e) => elementSelector(e)}
       />
     </Slate>
@@ -79,4 +95,52 @@ const CodeElement = (props) => {
 // DEFAULT element / p tag
 const DefaultElement = (props) => {
   return <p {...props.attributes}>{props.children}</p>;
+};
+
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    >
+      {props.children}
+    </span>
+  );
+};
+
+// =============  Custom  ============= //
+
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.bold === true,
+      universal: true,
+    });
+    return !!match;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    });
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: (n) => Editor.isBlock(editor, n) }
+    );
+  },
 };
