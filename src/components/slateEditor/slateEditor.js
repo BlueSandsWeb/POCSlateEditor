da7fdc9057b99cfa5d7f56991f1e672e3655ee17
JBernal <jon@bluesandswebdesign.com>
2020-06-15 16:08:37 -0700

    bold, italic, underline and strikethrough all working!

--- src/components/slateEditor/slateEditor.js
+++ src/components/slateEditor/slateEditor.js
@@ -43,6 +43,20 @@ const SlateEditor = () => {
         CustomEditor.toggleBoldMark(editor);
         break;
       }
+      case "i": {
+        CustomEditor.toggleItalicMark(editor);
+        break;
+      }
+      case "u": {
+        CustomEditor.toggleUnderlineMark(editor);
+        break;
+      }
+      case "s": {
+        if (e.shiftKey) {
+          CustomEditor.toggleStrikethroughMark(editor);
+        }
+        break;
+      }
     }
   };
 
@@ -62,6 +76,33 @@ const SlateEditor = () => {
           style={{ fontWeight: "bold" }}
         >
           B
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            CustomEditor.toggleItalicMark(editor);
+          }}
+          // style={{ color: 'red'}}
+        >
+          I
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            CustomEditor.toggleUnderlineMark(editor);
+          }}
+          // style={{ color: 'red'}}
+        >
+          U
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            CustomEditor.toggleStrikethroughMark(editor);
+          }}
+          // style={{ color: 'red'}}
+        >
+          S
         </button>
         <button
           onClick={(e) => {
@@ -101,16 +142,41 @@ const Leaf = (props) => {
   return (
     <span
       {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+      style={{
+        fontWeight: props.leaf.bold ? "bold" : "normal",
+        fontStyle: props.leaf.italic ? "italic" : "normal",
+        textDecoration: textDecorationSwitch(props.leaf),
+      }}
     >
       {props.children}
     </span>
   );
 };
 
+function textDecorationSwitch(leaf) {
+  console.log("leaf :>> ", leaf);
+  let decoration = "";
+  // strikethrough
+  if (leaf.strikethrough) {
+    decoration = "line-through";
+  }
+  // underline
+  if (leaf.underline) {
+    decoration += " underline";
+  }
+  console.log("decoration :>> ", decoration);
+  if (decoration.length === 0) {
+    return "none";
+  } else {
+    return decoration;
+  }
+}
+
 // =============  Custom  ============= //
 
 const CustomEditor = {
+  // =================== Check Activity Section =================== //
+  // ------------------- Check Text Decorations ------------------- //
   isBoldMarkActive(editor) {
     const [match] = Editor.nodes(editor, {
       match: (n) => n.bold === true,
@@ -119,6 +185,31 @@ const CustomEditor = {
     return !!match;
   },
 
+  isItalicMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.italic === true,
+      universal: true,
+    });
+    return !!match;
+  },
+
+  isUnderlineMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.underline === true,
+      universal: true,
+    });
+    return !!match;
+  },
+
+  isStrikethroughMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.strikethrough === true,
+      universal: true,
+    });
+    return !!match;
+  },
+
+  // ------------------- Check Elements ------------------- //
   isCodeBlockActive(editor) {
     const [match] = Editor.nodes(editor, {
       match: (n) => n.type === "code",
@@ -126,6 +217,9 @@ const CustomEditor = {
     return !!match;
   },
 
+  // =================== Toggle Section =================== //
+
+  // ------------------- Toggle Text Decorations ------------------- //
   toggleBoldMark(editor) {
     const isActive = CustomEditor.isBoldMarkActive(editor);
     Transforms.setNodes(
@@ -134,6 +228,35 @@ const CustomEditor = {
       { match: (n) => Text.isText(n), split: true }
     );
   },
+
+  toggleItalicMark(editor) {
+    const isActive = CustomEditor.isItalicMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { italic: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  toggleUnderlineMark(editor) {
+    const isActive = CustomEditor.isUnderlineMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { underline: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  toggleStrikethroughMark(editor) {
+    const isActive = CustomEditor.isStrikethroughMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { strikethrough: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  // ------------------- Toggle Elements ------------------- //
 
   toggleCodeBlock(editor) {
     const isActive = CustomEditor.isCodeBlockActive(editor);
